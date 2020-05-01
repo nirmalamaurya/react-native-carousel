b06ef539514b61bacccdd2f088a5a115522b1ffa
nirmala <anand.a.anand@capgemini.com>
2020-05-01 21:58:28 +1000

    added carousel

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,10 +1,42 @@
-import React from 'react';
-import {Text} from 'react-native';
+import React, { useEffect } from "react";
+import { Text, Dimensions, Image, View, StyleSheet } from "react-native";
+import Carousel from "react-native-banner-carousel";
+const Dashobard = () => {
 
+  const bannerWidth = Dimensions.get("window").width;
+  const bannerHeight = 260;
 
-const Dashobard = () =>{
+  const images = ["1", "2", "3", "4"];
 
-    return <Text>This is Dashboard</Text>;
+  
+
+  const displayImage = images.map((item, key) => (
+    <Image source={require(`../../assets/2.jpg`)} style={styles.image} />
+  ));
+
+  return (
+    <View style={styles.container}>
+      <Text>This is Dashboard</Text>
+      <Carousel
+        autoplay
+        autoplayTimeout={5000}
+        loop
+        index={0}
+        pageSize={bannerWidth}
+      >
+        {displayImage}
+      </Carousel>
+    </View>
+  );
 };
-
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+  },
+  image: {
+    width: 500,
+    height: 500,
+  },
+});
 export default Dashobard;
